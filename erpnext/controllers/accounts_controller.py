2fc57da451da873b1ea8065868754cdbb34ffb9f
Bevan Tony Medrano <bevantonymedrano@Bevans-MacBook-Air.local>
2022-08-25 17:17:35 +0800

    Fix(accounts_controller)Unable to add item_code using Update item

--- erpnext/controllers/accounts_controller.py
+++ erpnext/controllers/accounts_controller.py
@@ -2398,7 +2398,9 @@ def set_order_defaults(
 		get_conversion_factor(item.item_code, child_item.uom).get("conversion_factor")
 	)
 	child_item.conversion_factor = flt(trans_item.get("conversion_factor")) or conversion_factor
-	child_item.reqd_by_date = trans_item.get('reqd_by_date') or p_doc.reqd_by_date
+	
+	if parent_doctype == 'Sales Order':
+		child_item.reqd_by_date = trans_item.get('reqd_by_date') or p_doc.reqd_by_date
 
 	if child_doctype == "Purchase Order Item":
 		# Initialized value will update in parent validation
