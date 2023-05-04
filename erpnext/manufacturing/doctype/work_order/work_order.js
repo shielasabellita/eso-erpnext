52cd86f3121d33ccb667381c02706911039323d1
icecliff <isaiah@elexess.com>
2023-05-04 09:06:44 +0000

    fix(work_order.js): Add Material Consumption Button to Create Group

--- erpnext/manufacturing/doctype/work_order/work_order.js
+++ erpnext/manufacturing/doctype/work_order/work_order.js
@@ -669,8 +669,7 @@ erpnext.work_order = {
 							var consumption_btn = frm.add_custom_button(__('Material Consumption'), function() {
 								const backflush_raw_materials_based_on = frm.doc.__onload.backflush_raw_materials_based_on;
 								erpnext.work_order.make_consumption_se(frm, backflush_raw_materials_based_on);
-							});
-							consumption_btn.addClass('btn-primary');
+							}, __("Create"));
 						}
 					}
 				}
