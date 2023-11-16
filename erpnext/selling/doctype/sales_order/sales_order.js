01e2c52e94752394ef67d36c535c24c715feaa7e
Bevan Tony Medrano <medranobevantony@gmail.com>
2023-11-16 13:15:20 +0800

    fix(sales_order.js)Remap trigger of work order creation

--- erpnext/selling/doctype/sales_order/sales_order.js
+++ erpnext/selling/doctype/sales_order/sales_order.js
@@ -189,7 +189,10 @@ erpnext.selling.SalesOrderController = erpnext.selling.SellingController.extend(
 					// delivery note
 					if(flt(doc.per_delivered, 6) < 100 && (order_is_a_sale || order_is_a_custom_sale) && allow_delivery) {
 						this.frm.add_custom_button(__('Delivery Note'), () => this.make_delivery_note_based_on_delivery_date(), __('Create'));
-						this.frm.add_custom_button(__('Work Order'), () => this.make_work_order(), __('Create'));
+
+                        // changed for newmatik process.
+						// this.frm.add_custom_button(__('Work Order'), () => this.make_work_order(), __('Create'));
+						this.frm.add_custom_button(__('Work Order'), () => make_work_order_newmatik(), __('Create'));
 					}
 
 					// sales invoice
@@ -786,3 +789,97 @@ erpnext.selling.SalesOrderController = erpnext.selling.SellingController.extend(
 	}
 });
 $.extend(cur_frm.cscript, new erpnext.selling.SalesOrderController({frm: cur_frm}));
+
+var make_work_order_newmatik = function (frm) {
+    frappe.call({
+      method: "newmatik.api.sales_order.get_work_order_items",
+      args: {
+        items: cur_frm.doc.items,
+        sales_order: cur_frm.docname,
+        company: cur_frm.doc.company
+      },
+      callback: function(r) {
+        if(!r.message) {
+          frappe.msgprint({
+            title: __('Work Order not created'),
+            message: __('No Items with Bill of Materials to Manufacture'),
+            indicator: 'orange'
+          });
+          return;
+        }
+        else if(!r.message) {
+          frappe.msgprint({
+            title: __('Work Order not created'),
+            message: __('Work Order already created for all items with BOM'),
+            indicator: 'orange'
+          });
+          return;
+        } else {
+          const fields = [{
+            label: 'Items',
+            fieldtype: 'Table',
+            fieldname: 'items',
+            description: __('Select BOM and Qty for Production'),
+            fields: [{
+              fieldtype: 'Read Only',
+              fieldname: 'item_code',
+              label: __('Item Code'),
+              in_list_view: 1
+            }, {
+              fieldtype: 'Link',
+              fieldname: 'bom',
+              options: 'BOM',
+              reqd: 1,
+              label: __('Select BOM'),
+              in_list_view: 1,
+              get_query: function (doc) {
+                return { filters: { item: doc.item_code } };
+              }
+            }, {
+              fieldtype: 'Float',
+              fieldname: 'pending_qty',
+              reqd: 1,
+              label: __('Qty'),
+              in_list_view: 1
+            }, {
+              fieldtype: 'Link',
+              fieldname: 'company',
+              options: 'Company',
+              reqd: 1,
+              label: __('Company'),
+              in_list_view: 1
+            }, {
+              fieldtype: 'Data',
+              fieldname: 'sales_order_item',
+              reqd: 1,
+              label: __('Sales Order Item'),
+              hidden: 1
+            }],
+            data: r.message,
+            get_data: () => {
+              return r.message
+            }
+          }]
+          var d = new frappe.ui.Dialog({
+            title: __('Select Items to Manufacture'),
+            fields: fields,
+            primary_action: function() {
+              d.hide();
+              var data = d.get_values();
+              cur_frm.call({
+                method: 'newmatik.api.sales_order.make_work_orders',
+                args: {
+                  items: data,
+                  sales_order: cur_frm.docname
+                },
+                freeze: true,
+                freeze_message: __("Creating Work Orders")
+              });
+            },
+            primary_action_label: __('Create')
+          });
+          d.show();
+        }
+      }
+    });
+  }
